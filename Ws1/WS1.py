8ebd2185f0061f9b9ddfa44c1128a40cff79f030
Eliot Grimont <grimonteliot1@gmail.com>
2024-05-14 14:11:09 +0200

    Réalisation_Ws1

--- Ws1/WS1.py
+++ Ws1/WS1.py
@@ -1,1 +1,13 @@
-print("test")
+import random
+
+nombre = 0
+nb_random = random.randint(1, 100)
+
+while nb_random != nombre:
+    nombre = int(input("Entrez un nombre entre 1 et 100 : "))
+    if nombre > nb_random:
+        print('Trop grand')
+    elif nombre < nb_random:
+        print('Trop petit')
+
+print("Bien joué ! Le nombre était", nb_random)
